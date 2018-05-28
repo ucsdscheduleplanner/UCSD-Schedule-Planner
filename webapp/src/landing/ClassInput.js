8d95cefea2f97135e00d41507a86cfc5c404e3f0
Cameron <cameron.trando@yahoo.com>
2018-05-27 17:36:21 -0700

    Adding more error handling, working on reducing #JSON requests
    Using primereact to make popup occurs on error conditions and success
    conditions. Making it so that when we select a department we have
    selected before we will not make another api call.

--- webapp/src/landing/ClassInput.js
+++ webapp/src/landing/ClassInput.js
@@ -2,6 +2,8 @@ import React, {PureComponent} from 'react';
 import {Rating} from "primereact/components/rating/Rating";
 import {ListBox} from "primereact/components/listbox/ListBox";
 import {Button} from "primereact/components/button/Button";
+import {Messages} from 'primereact/components/messages/Messages';
+import {Message} from 'primereact/components/message/Message';
 import {AutoComplete} from "primereact/components/autocomplete/AutoComplete";
 import "../css/ClassInput.css";
 
@@ -33,7 +35,9 @@ export default class ClassInput extends PureComponent {
     }
 
     completeClassSuggestions(event) {
-        let classOptions = this.props.classes.filter((Class) => {
+        // hopefully this will never trigger
+        if (!this.props.classes[this.props.currentDepartment]) return;
+        let classOptions = this.props.classes[this.props.currentDepartment].filter((Class) => {
             return Class.toLowerCase().startsWith(event.query.toLowerCase());
         });
         this.setState({classOptions: classOptions});
@@ -46,7 +50,13 @@ export default class ClassInput extends PureComponent {
         this.setState({instructorOptions: instructorOptions});
     }
 
+    showError(message) {
+        this.message.show({ severity: 'error', summary: message, life: 750});
+    }
+
     handleSubmit() {
+        let error = false;
+        // doing pre-checking
         if (this.props.currentCourseNum === null || this.props.currentDepartment === null) return;
         let classTitle = `${this.props.currentDepartment} ${this.props.currentCourseNum}`;
         // testing whether this is a duplicate class
@@ -54,7 +64,11 @@ export default class ClassInput extends PureComponent {
             return accumulator || classTitle === previousClass['class_title']
         }, false);
 
-        if (!duplicate) {
+        // error checking on department and course num
+        if(!this.props.departments.includes(this.props.currentDepartment)) error = true;
+        if(!this.props.classes[this.props.currentDepartment].includes(this.props.currentCourseNum)) error = true;
+
+        if (!duplicate && !error) {
             // constructing new class to be added to UI
             let newClass = {};
             newClass['class_title'] = classTitle;
@@ -72,6 +86,8 @@ export default class ClassInput extends PureComponent {
             this.props.setCurrentCourseNum(null);
             this.props.setPriority(null);
             this.props.setConflicts(null);
+        } else {
+            this.showError("Failed to add class");
         }
         this.props.setUID(this.props.uid + 1);
         // set duplicate so we can do some UI stuff in case
@@ -102,8 +118,9 @@ export default class ClassInput extends PureComponent {
             newClass['conflicts'] = this.props.conflicts;
             newClass['instructor'] = this.props.currentInstructor;
 
-            // using the addClass method from the reducer
+            // using the edit method from the reducer
             this.props.editClass(this.props.editUID, newClass);
+            this.message.show({ severity: 'success', summary: 'Edit Successful', life: 750});
             this.props.exitEditMode();
         }
         // set duplicate so we can do some UI stuff in case
@@ -145,8 +162,10 @@ export default class ClassInput extends PureComponent {
             </div>
         );
 
+        let that = this;
         return (
             <React.Fragment>
+                <Messages ref={(el) => this.message = el} />
                 <div className="content">
                     <div className="form-field">
                         <div className="input-header"> Department:</div>
@@ -160,13 +179,24 @@ export default class ClassInput extends PureComponent {
                                       }}
                                       completeMethod={this.completeDepartmentSuggestions.bind(this)}
                                       onSelect={(e) => {
+                                          // don't requery if we have the class already
+                                          if (this.props.classes[e.value]) return;
                                           this.props.getClasses.call(this, e.value);
+                                          // check if the thing we selected is the same as the one we already had
                                           if (e.value !== this.props.currentDepartment) {
                                               this.props.setCurrentCourseNum(null);
                                               this.props.setCurrentInstructor(null);
                                               this.props.setPriority(null);
                                           }
-                                      }}/>
+                                      }}
+                                      onBlur={() => {
+                                          // don't requery if we have the class already
+                                          if (this.props.classes[this.props.currentDepartment]) return;
+                                          // don't query if it is not in our departments
+                                          if (!this.props.departments.includes(this.props.currentDepartment)) return;
+                                          this.props.getClasses.call(this, this.props.currentDepartment);
+                                      }}
+                        />
                     </div>
 
                     <div className="form-field">
@@ -189,8 +219,10 @@ export default class ClassInput extends PureComponent {
                                           }
                                       }}
                                       completeMethod={this.completeClassSuggestions.bind(this)}
-                                      disabled={this.props.currentDepartment === null
-                                      || this.props.currentDepartment.length === 0}
+                                      disabled={
+                                          this.props.currentDepartment === null
+                                          || this.props.currentDepartment.length === 0
+                                          || !this.props.departments.includes(this.props.currentDepartment)}
                                       dropdown={true}/>
                     </div>
 
@@ -199,7 +231,10 @@ export default class ClassInput extends PureComponent {
                     <div className="preference-container">
                         <div className="form-field">
                             <div className="input-header"> Instructor Preference:</div>
-                            <AutoComplete suggestions={this.props.instructorsPerClass[this.props.currentCourseNum]}
+                            <AutoComplete suggestions={
+                                // because to clear the thing we put undefined, we can just put the course num in whether
+                                // it is actually in the dict or not because if not it will be undefined and show nothing
+                                this.props.instructorsPerClass[this.props.currentCourseNum]}
                                           value={this.props.currentInstructor}
                                           onChange={(e) => this.props.setCurrentInstructor(e.value)}
                                           completeMethod={this.completeInstructorSuggestions.bind(this)}
@@ -208,7 +243,9 @@ export default class ClassInput extends PureComponent {
                         </div>
                         <div className="form-field ignore-class-types">
                             <div className="input-header"> Ignore Class Types:</div>
-                            <ListBox value={this.props.conflicts}
+                            <ListBox value={
+                                // same as above with the undefined
+                                this.props.conflicts}
                                      options={this.props.classTypesPerClass[this.props.currentCourseNum]}
                                      onChange={(e) => this.props.setConflicts(e.value)}
                                      multiple={true}
@@ -217,7 +254,9 @@ export default class ClassInput extends PureComponent {
 
                         <div className="form-field">
                             <div className="input-header"> Importance:</div>
-                            <Rating value={this.props.priority}
+                            <Rating value={
+                                // same as above with undefined
+                                this.props.priority}
                                     onChange={(e) => this.props.setPriority(e.value)}
                                     stars={3}/>
                         </div>
