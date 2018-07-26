8a08105ba705889db748716d324cd0f2a825bec6
Cameron <cameron.trando@yahoo.com>
2018-07-25 22:18:17 -0700

    Adding responsiveness to schedule

--- webapp/src/landing/ScheduleOptions.js
+++ webapp/src/landing/ScheduleOptions.js
@@ -4,6 +4,7 @@ import {Button} from "primereact/components/button/Button";
 import {Sidebar} from "primereact/components/sidebar/Sidebar";
 import {Calendar} from "primereact/components/calendar/Calendar";
 import {SelectButton} from "primereact/components/selectbutton/SelectButton";
+import MediaQuery from 'react-responsive';
 
 /*
     This class should hold the UI for settings options for the schedule.
@@ -38,7 +39,8 @@ export default class ScheduleOptions extends PureComponent {
             activeButton = (
                 <React.Fragment>
                     <div>
-                        <Button id="configure" style={{padding: "1em"}} onClick={this.toggleSideBar.bind(this)} label="Configure and Generate"/>
+                        <Button id="configure" style={{padding: "1em"}} onClick={this.toggleSideBar.bind(this)}
+                                label="Configure and Generate"/>
                     </div>
                 </React.Fragment>
             );
@@ -52,37 +54,57 @@ export default class ScheduleOptions extends PureComponent {
             {label: 'F', value: 'F'}
         ];
 
-        const scheduleOptionsComponent = (
-            <Sidebar id="sidebar" visible={this.state.visible} position="right"
-                     onHide={(e) => this.setState({visible: false})}>
-                <div className="schedule-options-sidebar">
-                    <div className="schedule-options-title"> Schedule Options</div>
-                    <div className="time-preference">
-                        <span className="time-preference-title">Time Preference:</span>
+        const scheduleOptionsContent = (
+            <div className="schedule-options-sidebar">
+                <div className="schedule-options-title"> Schedule Options</div>
+                <div className="time-preference">
+                    <span className="time-preference-title">Time Preference:</span>
 
-                        <div className="time-preference-start">
-                            Start:
-                            <Calendar value={this.props.startTimePreference} hourFormat="12" showTime={true} timeOnly={true}
-                                      onChange={(e) => this.props.setStartTimePreference(e.value)}/>
-                        </div>
-
-                        <div className="time-preference-end">
-                            End:
-                            <Calendar value={this.props.endTimePreference} hourFormat="12" showTime={true} timeOnly={true}
-                                      onChange={(e) => {this.props.setEndTimePreference(e.value)}}/>
-                        </div>
-                    </div>
-                    <div className="day-preference">
-                        <span className="day-preference-title">Day Preference:</span>
-
-                        <SelectButton id="day-preference" value={this.props.dayPreference} multiple={true} options={days}
-                                      onChange={(e) => this.props.setDayPreference(e.value)}/>
+                    <div className="time-preference-start">
+                        Start:
+                        <Calendar value={this.props.startTimePreference} hourFormat="12" showTime={true} timeOnly={true}
+                                  onChange={(e) => this.props.setStartTimePreference(e.value)}/>
                     </div>
 
-                    <Button id="generate" onClick={this.getSchedule.bind(this)} label="Generate"
-                            style={{padding: "1em"}}/>
+                    <div className="time-preference-end">
+                        End:
+                        <Calendar value={this.props.endTimePreference} hourFormat="12" showTime={true} timeOnly={true}
+                                  onChange={(e) => {
+                                      this.props.setEndTimePreference(e.value)
+                                  }}/>
+                    </div>
                 </div>
-            </Sidebar>
+                <div className="day-preference">
+                    <span className="day-preference-title">Day Preference:</span>
+
+                    <SelectButton id="day-preference" value={this.props.dayPreference} multiple={true} options={days}
+                                  onChange={(e) => this.props.setDayPreference(e.value)}/>
+                </div>
+
+                <Button id="generate" onClick={this.getSchedule.bind(this)} label="Generate"
+                        style={{padding: "1em"}}/>
+            </div>
+        );
+
+        const scheduleOptionsComponent = (
+            <MediaQuery query="(max-width: 525px)">
+                {(matches) => {
+                    if (matches) {
+                        return (
+                            <Sidebar id="sidebar" visible={this.state.visible} position="bottom"
+                                     onHide={(e) => this.setState({visible: false})}>
+                                {scheduleOptionsContent}
+                            </Sidebar>
+                        );
+                    } else return (
+                        <Sidebar id="sidebar" visible={this.state.visible} position="right"
+                                 onHide={(e) => this.setState({visible: false})}>
+                            {scheduleOptionsContent}
+                        </Sidebar>
+                    )
+                }}
+
+            </MediaQuery>
         );
 
         return (
@@ -90,7 +112,8 @@ export default class ScheduleOptions extends PureComponent {
                 <div className="schedule-options-container">
                     <div className="class-summary-title"> Class Information</div>
                     <div className="capes-info-text">
-                        This space will contain data from CAPES in the future. <br/><br/> Please be patient as I work on this.
+                        This space will contain data from CAPES in the future. <br/><br/> Please be patient as I work on
+                        this.
                     </div>
 
                     {activeButton}
