fb388bd5eae351bf55d964ae02dad68acbf05eba
ctrando <cameron.trando@yahoo.com>
2018-11-17 18:57:42 -0800

    #10: Making GCalendar config public because the information is already sent in fetch requests

--- webapp/src/utils/GCalendar.js
+++ webapp/src/utils/GCalendar.js
@@ -1,19 +1,23 @@
-import configs from "./config";
 import moment from 'moment';
 
+const CONFIG_KEY_CLIENT_ID = "client-id";
+const CONFIG_API_KEY = "api-key";
+const CONFIG_DISCOVERY_DOCS = "discoveryDocs";
+const CONFIG_SCOPES = "scopes";
+
+const configs = {
+    "client-id": "215311567185-hq5j6i1sfqd6uk75vkuug1hrnvq6kqa2.apps.googleusercontent.com",
+    "api-key": "AIzaSyCRoIa6XnnI1MhfuaOFSU4z7z7j5vt10ZM",
+    "discoveryDocs": ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+    "scopes": "https://www.googleapis.com/auth/calendar",
+};
+
+const GTIME_FORMAT = "YYYY-MM-DDTHH:mm:ssZ";
+const NUM_WEEKS_AHEAD = 5;
+const RECURRING_EVENT_RULE = "RRULE:FREQ=WEEKLY;COUNT=" + NUM_WEEKS_AHEAD;
+
 export function addEvents(subsections) {
-
-    const CONFIG_KEY_CLIENT_ID = "client-id";
-    const CONFIG_API_KEY = "api-key";
-    const CONFIG_DISCOVERYDOCS = "discoveryDocs";
-    const CONFIG_SCOPES = "scopes";
-
     let events = [];
-    const GTIME_FORMAT = "YYYY-MM-DDTHH:mm:ssZ";
-    const NUM_WEEKS_AHEAD = 5;
-    const RECURRING_EVENT_RULE = "RRULE:FREQ=WEEKLY;COUNT=" + NUM_WEEKS_AHEAD;
-
-    console.log(RECURRING_EVENT_RULE);
 
     for (let i = 0; i < subsections.length; i++) {
         const subsection = subsections[i];
@@ -38,7 +42,7 @@ export function addEvents(subsections) {
 
     const CLIENT_ID = configs[CONFIG_KEY_CLIENT_ID];
     const API_KEY = configs[CONFIG_API_KEY];
-    const DISCOVERY_DOCS = configs[CONFIG_DISCOVERYDOCS];
+    const DISCOVERY_DOCS = configs[CONFIG_DISCOVERY_DOCS];
     const SCOPES = configs[CONFIG_SCOPES];
 
     async function addToGCalendar(isSignedIn) {
