57a372de23cb52bd41dd56fff85d081bd9ed32c1
snowme34 <snowme34@gmail.com>
2018-12-04 21:45:21 -0800

    modifying docker-compose, adding nginx container, tested nginx locally

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 function depends {
   if type $1 >/dev/null 2>&1; then
     echo "✓ Found $1."
@@ -15,10 +13,31 @@ if [ ! -d ".git" ]; then
   exit 1
 fi
 
+if [ ! -f "docker-compose.yml" ]; then 
+  echo "Error: docker-compose.yml missing, which is required for docker-compose"
+  echo "Please check the repo for missing files"
+  exit 1
+fi
+
+while test $# -gt 0; do
+   case "$1" in 
+       -h | --help)
+          echo "-h, --help        Gives the possible commands"
+          echo "-d, --download    Will download data fresh from Schedule of Classes"
+          exit 0
+          ;;
+      -d | --download)
+          export SDSCHEDULE_SCRAPE=1
+          shift
+          ;;
+      *)
+          break
+          ;;
+  esac
+done
+
 depends docker
 depends docker-compose
-
-export SDSCHEDULE_SCRAPE=1
 
 docker-compose build
 docker-compose up 
