b01acb5652bb11d12dd06f33213644c7dead33c6
David Hacker <dmhacker@protonmail.com>
2018-12-06 12:12:01 -0800

    Recombine webreg_scrape and webreg_upload files into one - better error handling

---
+++ backend/datautil/webreg_scrape_upload.py
@@ -0,0 +1,26 @@
+from scraper.departmentscraper import DepartmentScraper
+from scraper.scraper import Scraper
+from datautil.data_cleaner import Cleaner
+from datautil.data_parser import Parser
+from datautil.sqlite_to_mysql import export_to_mysql
+
+import sys
+
+ds = DepartmentScraper()
+ds.scrape()
+
+sc = Scraper()
+sc.scrape()
+
+if sc.crashed:
+    print("The scraper has crashed.", file=sys.stderr)
+    print("Make sure you have a good Internet connection before scraping.", file=sys.stderr)
+    sys.exit(1)
+else:
+    parser = Parser()
+    parser.parse()
+
+    cleaner = Cleaner()
+    cleaner.clean()
+
+    export_to_mysql()
