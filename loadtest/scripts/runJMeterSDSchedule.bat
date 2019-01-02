ec31e1f2c5cd180a635a89c61a75d12a44e86072
snowme34 <snowme34@gmail.com>
2019-01-02 10:53:13 -0800

    update load test plan #56

--- loadtest/scripts/runJMeterSDSchedule.bat
+++ loadtest/scripts/runJMeterSDSchedule.bat
@@ -5,4 +5,6 @@ rem env should be set in setenv.bat located at JMETER_HOME
 setlocal
 set "HEAP=-Xms1g -Xmx2g -XX:MaxMetaspaceSize=256m"
 
-C:\jmeter\bin\jmeter.bat -n -t C:\DC_L\project\UCSD-Planner-Helper\loadtest\scripts\SDScheduleDBTest.jmx -l C:\DC_L\project\UCSD-Planner-Helper\loadtest\output\SDSDBTresult.jtl -o C:\DC_L\project\UCSD-Planner-Helper\loadtest\output\report -e -Jthreads=100 -Jrampup=200 -Jloops=50 -Jhost=localhost -Jport=22125
+C:\jmeter\bin\jmeter.bat -n -t C:\DC_L\project\UCSD-Planner-Helper\loadtest\scripts\SDScheduleDBTest.jmx -l C:\DC_L\project\UCSD-Planner-Helper\loadtest\output\SDSDBTresult.jtl -o C:\DC_L\project\UCSD-Planner-Helper\loadtest\output\report -e -Jthreads=1500 -Jrampup=200 -Jloops=5 -Jhost=localhost -Jport=22125
+
+rem it seems that JMeter will creat a log file in the directory where the command is executed
