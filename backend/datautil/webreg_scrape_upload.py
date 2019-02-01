ffd476d20f8c13cf5cc911ef1bd7957a7f13078c
David Hacker <dmhacker@protonmail.com>
2019-01-31 22:20:10 -0800

    Print execution times from webreg_scrape_upload file

--- backend/datautil/webreg_scrape_upload.py
+++ backend/datautil/webreg_scrape_upload.py
@@ -7,32 +7,50 @@ from datautil.data_parser import Parser
 from datautil.sqlite_to_mysql import export_to_mysql
 
 import sys
+import time
+import pprint
 
 def main():
+    execution_times = {}
+
+    timestamp = time.time() 
     department_scraper = DepartmentScraper()
     department_scraper.scrape()
+    execution_times['Department Scraping'] = '{0:.3f} minutes'.format(time.time() - timestamp)
 
+    timestamp = time.time() 
     capes_scraper = CAPESScraper()
     capes_scraper.scrape()
+    execution_times['CAPES Scraping'] = '{0:.3f} minutes'.format(time.time() - timestamp)
 
     if capes_scraper.crashed:
         print("The CAPES scraper has crashed. Please retry.", file=sys.stderr)
         sys.exit(1)
 
+    timestamp = time.time() 
     course_scraper = CourseScraper()
     course_scraper.scrape()
+    execution_times['Course Scraping'] = '{0:.3f} minutes'.format(time.time() - timestamp)
 
     if course_scraper.crashed:
         print("The course scraper has crashed. Please retry.", file=sys.stderr)
         sys.exit(1)
 
+    timestamp = time.time() 
     parser = Parser()
     parser.parse()
+    execution_times['Course Parsing'] = '{0:.3f} minutes'.format(time.time() - timestamp)
 
+    timestamp = time.time() 
     cleaner = Cleaner()
     cleaner.clean()
+    execution_times['Cleaning'] = '{0:.3f} minutes'.format(time.time() - timestamp)
 
+    timestamp = time.time() 
     export_to_mysql()
+    execution_times['MySQL Exporting'] = '{0:.3f} minutes'.format(time.time() - timestamp)
+
+    pprint.pprint(execution_times)
 
 if __name__ == "__main__":
     main()
