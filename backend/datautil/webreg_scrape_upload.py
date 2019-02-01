bee6a5522321beb5404b8eaf5ac12795d0d894f7
David Hacker <dmhacker@protonmail.com>
2019-01-31 22:50:22 -0800

    Rename some course parsing files, add preliminary capes parsing content

--- backend/datautil/webreg_scrape_upload.py
+++ backend/datautil/webreg_scrape_upload.py
@@ -2,8 +2,9 @@ from scraper.department_scraper import DepartmentScraper
 from scraper.capes_scraper import CAPESScraper 
 from scraper.course_scraper import CourseScraper
 
+from datautil.capes_parser import CAPESParser
+from datautil.data_parser import CourseParser
 from datautil.data_cleaner import Cleaner
-from datautil.data_parser import Parser
 from datautil.sqlite_to_mysql import export_to_mysql
 
 import sys
@@ -16,39 +17,44 @@ def main():
     timestamp = time.time() 
     department_scraper = DepartmentScraper()
     department_scraper.scrape()
-    execution_times['Department Scraping'] = '{0:.3f} minutes'.format(time.time() - timestamp)
+    execution_times['Department Scraping'] = '{0:.3f} minutes'.format((time.time() - timestamp) / 60)
 
     timestamp = time.time() 
     capes_scraper = CAPESScraper()
     capes_scraper.scrape()
-    execution_times['CAPES Scraping'] = '{0:.3f} minutes'.format(time.time() - timestamp)
+    execution_times['CAPES Scraping'] = '{0:.3f} minutes'.format((time.time() - timestamp) / 60)
 
     if capes_scraper.crashed:
         print("The CAPES scraper has crashed. Please retry.", file=sys.stderr)
         sys.exit(1)
-
+    
     timestamp = time.time() 
-    course_scraper = CourseScraper()
+    course_scraper = CourseScraper() 
     course_scraper.scrape()
-    execution_times['Course Scraping'] = '{0:.3f} minutes'.format(time.time() - timestamp)
+    execution_times['Course Scraping'] = '{0:.3f} minutes'.format((time.time() - timestamp) / 60)
 
     if course_scraper.crashed:
         print("The course scraper has crashed. Please retry.", file=sys.stderr)
         sys.exit(1)
 
+    # timestamp = time.time() 
+    # parser = CAPESParser()
+    # parser.parse()
+    # execution_times['CAPES Parsing'] = '{0:.3f} minutes'.format((time.time() - timestamp) / 60)
+
     timestamp = time.time() 
-    parser = Parser()
+    parser = CourseParser()
     parser.parse()
-    execution_times['Course Parsing'] = '{0:.3f} minutes'.format(time.time() - timestamp)
+    execution_times['Course Parsing'] = '{0:.3f} minutes'.format((time.time() - timestamp) / 60)
 
     timestamp = time.time() 
     cleaner = Cleaner()
     cleaner.clean()
-    execution_times['Cleaning'] = '{0:.3f} minutes'.format(time.time() - timestamp)
+    execution_times['Cleaning'] = '{0:.3f} minutes'.format((time.time() - timestamp) / 60)
 
     timestamp = time.time() 
     export_to_mysql()
-    execution_times['MySQL Exporting'] = '{0:.3f} minutes'.format(time.time() - timestamp)
+    execution_times['MySQL Exporting'] = '{0:.3f} minutes'.format((time.time() - timestamp) / 60) 
 
     pprint.pprint(execution_times)
 
